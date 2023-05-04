a1d0cf245ecf4b2e2d6f1a30528ed90d72be2b59
BrandonTeker <82454837+Delta190@users.noreply.github.com>
2023-05-05 00:08:08 +0100

    Added Products table to DB

--- lib/product.dart
+++ lib/product.dart
@@ -5,53 +5,53 @@ part 'product.g.dart';
 @HiveType(typeId: 0)
 class Product extends HiveObject {
   @HiveField(0)
-  String name;
+  late String name;
 
   @HiveField(1)
-  String type;
+  late String type;
 
   @HiveField(2)
-  String description;
+  late String description;
 
   @HiveField(3)
-  String brand;
+  late String brand;
 
   @HiveField(4)
-  double weight;
+  late double weight;
 
   @HiveField(5)
-  double price;
+  late double price;
 
   @HiveField(6)
-  List<String> tags;
+  late List<String> tags;
 
   @HiveField(7)
-  double energyKJ;
+  late double energyKJ;
 
   @HiveField(8)
-  double energyKcal;
+  late double energyKcal;
 
   @HiveField(9)
-  double fat;
+  late double fat;
 
   @HiveField(10)
-  double saturates;
+  late double saturates;
 
   @HiveField(11)
-  double carbohydrate;
+  late double carbohydrate;
 
   @HiveField(12)
-  double sugars;
+  late double sugars;
 
   @HiveField(13)
-  double fibre;
+  late double fibre;
 
   @HiveField(14)
-  double protein;
+  late double protein;
 
   @HiveField(15)
-  double salt;
+  late double salt;
 
   @HiveField(16)
-  List<String> ingredients;
+  late List<String> ingredients;
 }
---
+++ lib/product.g.dart
@@ -0,0 +1,88 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductAdapter extends TypeAdapter<Product> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Product read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Product()
+      ..name = fields[0] as String
+      ..type = fields[1] as String
+      ..description = fields[2] as String
+      ..brand = fields[3] as String
+      ..weight = fields[4] as double
+      ..price = fields[5] as double
+      ..tags = (fields[6] as List).cast<String>()
+      ..energyKJ = fields[7] as double
+      ..energyKcal = fields[8] as double
+      ..fat = fields[9] as double
+      ..saturates = fields[10] as double
+      ..carbohydrate = fields[11] as double
+      ..sugars = fields[12] as double
+      ..fibre = fields[13] as double
+      ..protein = fields[14] as double
+      ..salt = fields[15] as double
+      ..ingredients = (fields[16] as List).cast<String>();
+  }
+
+  @override
+  void write(BinaryWriter writer, Product obj) {
+    writer
+      ..writeByte(17)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.type)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.brand)
+      ..writeByte(4)
+      ..write(obj.weight)
+      ..writeByte(5)
+      ..write(obj.price)
+      ..writeByte(6)
+      ..write(obj.tags)
+      ..writeByte(7)
+      ..write(obj.energyKJ)
+      ..writeByte(8)
+      ..write(obj.energyKcal)
+      ..writeByte(9)
+      ..write(obj.fat)
+      ..writeByte(10)
+      ..write(obj.saturates)
+      ..writeByte(11)
+      ..write(obj.carbohydrate)
+      ..writeByte(12)
+      ..write(obj.sugars)
+      ..writeByte(13)
+      ..write(obj.fibre)
+      ..writeByte(14)
+      ..write(obj.protein)
+      ..writeByte(15)
+      ..write(obj.salt)
+      ..writeByte(16)
+      ..write(obj.ingredients);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
