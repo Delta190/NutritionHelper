c3c4331c95c6f200eb9e13368ada65a1f376887f
Speedy-Speedy <36712145+Speedy-Speedy@users.noreply.github.com>
2023-05-10 21:22:16 +0100

    created tag filter page
    purpose of this page is to only display items with that tag

---
+++ lib/screens/tag_filter_page.dart
@@ -0,0 +1,131 @@
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+import 'package:nutritionhelperuimodule/hivetables/producttable.dart';
+import 'package:nutritionhelperuimodule/screens/account.dart';
+import 'package:nutritionhelperuimodule/screens/favourites.dart';
+import 'package:nutritionhelperuimodule/screens/itemdisplay.dart';
+import 'package:nutritionhelperuimodule/screens/nutritionlog.dart';
+import 'package:nutritionhelperuimodule/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:nutritionhelperuimodule/hivetables/tableloader.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  int _currentIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+
+      if (index == 0) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      } else if (index == 1) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => NutritionLog()));
+      } else if (index == 2) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => FavouritePage()));
+      } else if (index == 3) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AccountPage()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffe8e2e2),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.edit), label: "Log"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "Favourites"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
+        ],
+        backgroundColor: Color(0xffffffff),
+        currentIndex: _currentIndex,
+        elevation: 8,
+        iconSize: 24,
+        selectedItemColor: Color(0xff23bf49),
+        unselectedItemColor: Color(0xff9e9e9e),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        onTap: (value) {},
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  padding: EdgeInsets.all(0),
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xffffffff),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "<tag>",
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          color: Color(0xff000000),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.all(0),
+                    shrinkWrap: false,
+                    physics: ScrollPhysics(),
+                    children: [
+                      Text(
+                        "(Temporary) Used to display all data then the inout in the search bar will filter",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          color: Color(0xff000000),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
